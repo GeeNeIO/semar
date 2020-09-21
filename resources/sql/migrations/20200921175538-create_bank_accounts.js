f4b0781ecf723c067c212d1fd4181d65455a1196
elr0nda <elr0nda@geene.io>
2020-09-22 01:27:55 +0700

    chore(bank-accounts): add migration file

---
+++ resources/sql/migrations/20200921175538-create_bank_accounts.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const Promise = require('bluebird');
+
+module.exports = {
+  up: async (qi, { DataTypes }) => qi.createTable('bankAccounts', {
+    bankId: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    fkTableName: {
+      type: DataTypes.STRING,
+      alowNull: false,
+    },
+    fkTableId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    accountName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    accountNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    bankName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdTime: DataTypes.DATE,
+    updatedTime: DataTypes.DATE,
+  }).then(() => Promise.all([
+    qi.addIndex('bankAccounts', [
+      'fkTableName',
+      'fkTableId',
+    ], {
+      fields: [
+        'fkTableName',
+        'fkTableId',
+      ],
+    })
+  ])),
+
+  down: async (qi, Sequelize) => qi.dropTable('bankAccounts')
+};
