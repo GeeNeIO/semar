54b5d1522f25d65d510954f14f83f14489da3bf0
elr0nda <elr0nda@geene.io>
2020-09-22 23:51:21 +0700

    feat(bank-account): create BankAccount service skeleton

--- src/modules/bank-accounts/bank-accounts.module.ts
+++ src/modules/bank-accounts/bank-accounts.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BankAccountsController } from './bank-accounts.controller';
+import { BankAccountsService } from './bank-accounts.service';
 
 @Module({
-  controllers: [BankAccountsController]
+  controllers: [BankAccountsController],
+  providers: [BankAccountsService]
 })
 export class BankAccountsModule {}
---
+++ src/modules/bank-accounts/bank-accounts.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Observable, of } from 'rxjs';
+import { BankAccountModel } from './entities/bank-account.entity';
+import { BankAccount } from './types/bank-account.types';
+
+@Injectable()
+export class BankAccountsService {
+  constructor(
+    @InjectModel(BankAccountModel)
+    private readonly bankAccountRepository: typeof BankAccountModel,
+  ) { }
+
+  createAccount(account: BankAccount & {
+    fkTableName: string,
+    fkTableId: string,
+  }): Observable<BankAccount> {
+    return of(null);
+  }
+
+  updateAccount(
+    bankId: string, 
+    account: Partial<BankAccount>): Observable<BankAccount> {
+    return of(null);
+  }
+
+  deleteAccount(bankId: string): Observable<BankAccount> {
+    return of(null);
+  }
+
+  getAccount(bankId: string): Observable<BankAccount> {
+    return of(null);
+  }
+
+  getAccounts(fkTableName: string, fkTableId: string): Observable<BankAccount[]> {
+    return of(null);
+  }
+
+}
