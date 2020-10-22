d8e742211908631b45e72bf3829a69b428bbe25a
elr0nda <elr0nda@geene.io>
2020-10-23 02:43:41 +0700

    fix(app): handle cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.HTTP_PORT || 3000);
 }
 bootstrap();
